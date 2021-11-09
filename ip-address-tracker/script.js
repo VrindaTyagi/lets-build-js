8957967a165bcd8d3d79949e20d0039ef4aa835d
Vrinda Tyagi <vrindatyagi09@gmail.com>
2021-11-09 13:16:53 +0530

    js implementation done

--- ip-address-tracker/script.js
+++ ip-address-tracker/script.js
@@ -1,42 +1,54 @@
-var data = [];
+const ipAddress = document.getElementById("dyn-ip");
+const ipLocation = document.getElementById("dyn-loc");
+const timezone = document.getElementById("dyn-tzone");
+const isp = document.getElementById("dyn-isp");
+const input = document.getElementById("input-value")
 
-const fetchData = () => {
+var mymap = L.map("map");
 
-    fetch(
-        "https://geo.ipify.org/api/v2/country,city?apiKey=at_dxe2UUX2g4ZnJF713lp8H70FEtjLt&ipAddress=122.161.66.63"
-      )
-        .then((res) => res.json())
-        .then((res) => {
-          data = res;
-          console.log(data);
-          const {location} = data;
-          newLoc(location.lat, location.lng);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+let lat;
+let lng;
 
-}
+const fetchData = (val = "") => {
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_dxe2UUX2g4ZnJF713lp8H70FEtjLt&ipAddress=${val}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      lat = data.location.lat;
+      lng = data.location.lng;
+      displayData(data);
+      newLoc();
+      // input.innerText = "";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 window.onload = fetchData();
 
-const newLoc = (lat, lng) => {
-  const mymap = L.map('map').setView([lat, lng], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidnJpbmRhLXR5YWdpMDkiLCJhIjoiY2t2cTg5eW5tMXdjczJ1bzV6cHF0NzNmcCJ9.mR1IsOMZHMNcWq39Iv1W_g', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(mymap);
+const displayData = (data) => {
+  ipAddress.innerText = data.ip;
+  ipLocation.innerText = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+  timezone.innerText = `UTC ${data.location.timezone}`;
+  isp.innerText = data.isp;
+};
 
+const newLoc = () => {
+  mymap.setView([lat, lng], 13);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidnJpbmRhLXR5YWdpMDkiLCJhIjoiY2t2cTg5eW5tMXdjczJ1bzV6cHF0NzNmcCJ9.mR1IsOMZHMNcWq39Iv1W_g",
+    {
+      maxZoom: 18,
+      attribution: false,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    }
+  ).addTo(mymap);
   L.marker([lat, lng]).addTo(mymap);
-}
-
-
-
-    // var marker = L.marker().addTo(mymap);
-
+};
 
 
